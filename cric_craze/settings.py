1ab003f968c53403bb9f3ff97e71c617cf8adb68
smartcoderrahis <rahissheikh200@gmail.com>
2025-11-10 16:25:39 +0545

    added requirments.txt

--- cric_craze/settings.py
+++ cric_craze/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lp(q^o4vs%^mg91+&2i5)!*=djga4q3ec+@e28vkdfx_=atq4@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.82', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.82', 'localhost', '127.0.0.1', 'smartcoderrahis.pythonanywhere.com']
 
 
 # Application definition
